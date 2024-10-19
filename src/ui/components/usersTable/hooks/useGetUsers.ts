1ca41716db16f7b8680dce23b0237046e6e6de7e
Ororura <hitmanlol890@gmail.com>
2024-10-19 16:31:12 +0300

    code refactoring

--- src/ui/components/usersTable/hooks/useGetUsers.ts
+++ src/ui/components/usersTable/hooks/useGetUsers.ts
@@ -1,19 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
-import { UserType } from 'components/usersTable/types.ts';
-import { usersApi } from 'services/query';
+import { UserType } from 'components/usersTable/types.ts'; // Обновите путь на актуальный
 import { userStore } from 'core/store';
+import { usersApi } from 'services/query';
 
 const useGetUsers = () => {
-  return useQuery({
-    queryKey: [''],
+  return useQuery<UserType[], Error>({
+    queryKey: ['users'],
     queryFn: async () => {
-      const { data } = await usersApi.getUserData<UserType[]>();
-      if (data && data.length > 0) {
-        userStore.setState(() => {
-          return {
+      try {
+        const { data } = await usersApi.getUserData<UserType[]>();
+        if (data && data.length > 0) {
+          userStore.setState(() => ({
             ['users']: data,
-          };
-        });
+          }));
+        } else {
+          return [];
+        }
+
+        return data;
+      } catch (error) {
+        console.error('Ошибка получения данных:', error);
+        throw error;
       }
     },
   });
